d4652b9ccbce5418fdeca1e68c93925f89f9d4fa
Typicode <typicode@gmail.com>
2014-06-23 15:32:17 +0200

    Fix

--- bin/install.js
+++ bin/install.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 // Run when package is installed
+var fs = require('fs')
 var husky = require('../src/')
 
 var dir = __dirname + '/../../../.git/hooks'
@@ -7,7 +8,10 @@ var dir = __dirname + '/../../../.git/hooks'
 console.log('\033[36m%s\033[0m', 'husky')
 console.log('  setting up hooks in .git/hooks/')
 
-husky.create(dir, 'pre-commit', 'npm run precommit')
-husky.create(dir, 'pre-push', 'npm run prepush')
-
-console.log('  done\n')
+if (fs.existsSync(dir)) {
+  husky.create(dir, 'pre-commit', 'npm run precommit')
+  husky.create(dir, 'pre-push', 'npm run prepush')
+  console.log('  done\n')
+} else {
+  console.log('  can\'t find .git/hooks/')
+}
