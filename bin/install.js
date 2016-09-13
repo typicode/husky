992e4cb22b6ae0626dc8c12aa0d2fbe5c653bf7d
typicode <typicode@gmail.com>
2016-09-13 22:24:59 +0200

    Skip Git hooks installation in CI

--- bin/install.js
+++ bin/install.js
@@ -1,7 +1,14 @@
 // Run when package is installed
 var fs    = require('fs')
+var isCI  = require('is-ci')
 var husky = require('../src/')
 var hooks = require('../src/hooks.json')
+
+if (isCI) {
+  console.log('\033[4;36m%s\033[0m', 'husky')
+  console.log('CI detected, skipping Git hooks installation')
+  return
+}
 
 console.log('\033[4;36m%s\033[0m', 'husky')
 console.log('setting up hooks in .git/hooks')
