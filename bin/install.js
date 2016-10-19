58b39041e494d2ed24ceb0ab91ea4268db48dc84
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-10-19 22:41:46 +0200

    Properly walk up the tree for a package.json file

--- bin/install.js
+++ bin/install.js
@@ -1,11 +1,9 @@
 // Run when package is installed
 var fs       = require('fs')
 var isCI     = require('is-ci')
-var findRoot = require('find-root')
+var path     = require('path')
 var husky    = require('../src/')
 var hooks    = require('../src/hooks.json')
-
-var package  = findRoot('package.json')
 
 if (isCI) {
   console.log('\033[4;36m%s\033[0m', 'husky')
@@ -16,12 +14,34 @@ if (isCI) {
 console.log('\033[4;36m%s\033[0m', 'husky')
 console.log('setting up hooks in .git/hooks')
 
+function getPackageJson() {
+  var filePath = process.cwd().split(path.sep);
+
+  while(filePath.length > 1) {
+    var search = path.join(filePath.join(path.sep), 'package.json');
+
+    try {
+      if (fs.statSync(search)) {
+        var json = require(search)
+        if (json.config && json.config.husky) {
+          return json;
+        }
+      }
+    } catch (e) {}
+
+    filePath.pop();
+  }
+
+  return null;
+}
+
 husky.hooksDir(function(err, dir) {
   if (err) {
     console.error('  ' + err)
   } else {
-    var config = package && package.config && package.config.husky
-      ? package.config.husky
+    var json = getPackageJson();
+    var config = json !== null && json.config && json.config.husky
+      ? json.config.husky
       : {};
 
     hooks.forEach(function (hook) {
