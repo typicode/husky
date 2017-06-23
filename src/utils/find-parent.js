f754ca52398a86fdf8b1ace0e3cde4a6dbd218cf
typicode <typicode@gmail.com>
2017-06-23 23:10:49 +0200

    Fix

--- src/utils/find-parent.js
+++ src/utils/find-parent.js
@@ -4,13 +4,13 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = function findParent(currentDir, name) {
-  const dirs = currentDir.split('/')
+  const dirs = currentDir.split(path.sep)
 
   while (dirs.pop()) {
-    const dir = dirs.join('/')
+    const dir = dirs.join(path.sep)
 
-    if (fs.existsSync(`${dir}/${name}`)) {
-      return path.resolve(`${dir}/`)
+    if (fs.existsSync(path.join(dir, name))) {
+      return path.resolve(dir)
     }
   }
 }
