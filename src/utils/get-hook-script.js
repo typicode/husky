04064be0fb515323a783282626ddf0b480479db3
typicode <typicode@gmail.com>
2017-06-24 16:29:34 +0200

    Update

--- src/utils/get-hook-script.js
+++ src/utils/get-hook-script.js
@@ -11,32 +11,55 @@ function platformSpecific() {
     // Node standard installation path /c/Program Files/nodejs
     // for GUI apps
     // https://github.com/typicode/husky/issues/49
-    return 'export PATH="$PATH:/c/Program Files/nodejs"'
+    return stripIndent(
+      `
+      # Node standard installation
+      export PATH="$PATH:/c/Program Files/nodejs"'`
+    )
   } else {
-    // Add
-    // Brew standard installation path /usr/local/bin
-    // Node standard installation path /usr/local
-    // for GUI apps
-    // https://github.com/typicode/husky/issues/49
+    // Using normalize to support ' in path
+    // https://github.com/typicode/husky/issues/117
+    const home = normalize(process.env.HOME)
+
     const arr = [
-      stripIndent(`
+      stripIndent(
+        `
         load_nvm () {
-          export $1=$2
-          command_exists nvm || [ -s "$2/nvm.sh" ] && . $2/nvm.sh
+          # If nvm is not loaded, load it
+          command_exists nvm || {
+            export NVM_DIR=${home}/.nvm
+            [ -s "$1/nvm.sh" ] && . $1/nvm.sh
+          }
+
+          # If nvm has been loaded correctly, use project .nvmrc
           command_exists nvm && [ -f .nvmrc ] && nvm use
         }
 
-        export PATH=$PATH:/usr/local/bin:/usr/local`)
+        # Add common path where Node can be found
+        # Brew standard installation path /usr/local/bin
+        # Node standard installation path /usr/local
+        export PATH=$PATH:/usr/local/bin:/usr/local`
+      )
     ]
 
+    arr.push(
+      stripIndent(
+        `
+        # nvm path with standard installation
+        load_nvm ${home}/.nvm`
+      )
+    )
+
     if (process.platform === 'darwin') {
-      // Load nvm with BREW_NVM_DIR set to /usr/local/opt/nvm
-      arr.push('load_nvm BREW_NVM_DIR /usr/local/opt/nvm')
+      arr.push(
+        stripIndent(
+          `
+          # nvm path installed with Brew
+          load_nvm /usr/local/opt/nvm`
+        )
+      )
     }
 
-    // Load nvm with NVM_DIR set to $HOME/.nvm
-    // ~ is unavaible, so $HOME is used
-    arr.push(`load_nvm NVM_DIR ${process.env.HOME}/.nvm`)
     return arr.join('\n')
   }
 }
@@ -60,34 +83,35 @@ module.exports = function getHookScript(hookName, relativePath, npmScriptName) {
         command -v "$1" >/dev/null 2>&1
       }
 
-      # https://github.com/typicode/husky/issues/76
       has_hook_script () {
         [ -f package.json ] && cat package.json | grep -q "\\"$1\\"[[:space:]]*:"
       }
 
       cd ${normalizedPath}
 
+      # Check if ${npmScriptName} script is defined, skip if not
       has_hook_script ${npmScriptName} || exit 0`
     ).trim(),
     platformSpecific(),
     stripIndent(
       `
+      # Check that npm exists
       command_exists npm || {
-        echo >&2 "husky > Can't find npm in PATH. Skipping ${npmScriptName} script in package.json"
+        echo >&2 "husky > can't find npm in PATH, skipping ${npmScriptName} script in package.json"
         exit 0
       }
 
-      # Expose GIT params
+      # Export Git hook params
       export GIT_PARAMS="$*"
 
-      # Run script
-      echo "husky > npm run -s ${npmScriptName} (node \`node -v\`)"
+      # Run npm script
+      echo "husky > npm run -s ${npmScriptName} (node \`node -v\`)\\n"
       echo
       npm run -s ${npmScriptName} || {
-        echo
-        echo "husky > ${hookName} hook failed ${noVerifyMessage}"
+        echo "\\nhusky > ${hookName} hook failed ${noVerifyMessage}"
         exit 1
-      }`
-    ).trim()
+      }
+      `
+    )
   ].join('\n')
 }
