adcad9d71684f0a07e9b75467d48164d648c2e95
typicode <typicode@gmail.com>
2017-06-27 20:43:18 +0200

    Update install.js

--- bin/install.js
+++ bin/install.js
@@ -6,12 +6,12 @@ const isCI = require('is-ci')
 const installFrom = require('../src/install')
 
 console.log('husky')
-
+/*
 if (isCI) {
   console.log('CI detected, skipping Git hooks installation')
   process.exit(0)
 }
-
+*/
 console.log('setting up Git hooks')
 
 const huskyDir = path.join(__dirname, '..')
