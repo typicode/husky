f6b61128c4ecd45c656711debc1d2b74da6cee0f
Sergei Tarassov <starassov@leongaming.com>
2017-09-04 14:23:09 +0300

    Add scope feature

--- src/install.js
+++ src/install.js
@@ -29,11 +29,64 @@ function createHook(huskyDir, hooksDir, hookName, cmd) {
   // When used in submodule, the project dir is the first .git that is found
   const projectDir = findParent(huskyDir, '.git')
 
+  const config = {
+    scope: null
+  }
+
+  const configPath = path.resolve(packageDir, '.huskyrc')
+  if (fs.existsSync(configPath)) {
+    // Check `.huskyrc` json file for husky options.
+    try {
+      Object.assign(config, JSON.parse(fs.readFileSync(configPath)))
+    } catch (error) {
+      error.message =
+        '[husky] Unable to create git hook. Config file `.huskyrc` is invalid json: ' +
+        error.message
+      throw error
+    }
+  } else {
+    // Check package.json for husky options.
+    const pkgPath = path.resolve(packageDir, 'package.json')
+    if (fs.existsSync(pkgPath)) {
+      const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
+      if (typeof pkg.husky === 'object' && pkg.husky !== null) {
+        Object.assign(config, pkg.husky)
+      }
+    }
+  }
+
   // In order to support projects with package.json in a different directory
   // than .git, find relative path from project directory to package.json
   const relativePath = path.join('.', path.relative(projectDir, packageDir))
 
-  const hookScript = getHookScript(hookName, relativePath, cmd)
+  // Check husky scope.
+  if (config.scope === null) {
+    config.scope = ''
+  } else {
+    console.assert(
+      typeof config.scope === 'string',
+      '[husky] Expected scope to be a string: ' + config.scope
+    )
+    // Find relative path from package.json directory to scope path.
+    config.scope = path.relative(
+      projectDir,
+      path.join(packageDir, config.scope)
+    )
+    console.assert(
+      config.scope.indexOf('..') !== 0,
+      '[husky] Scope path is outside of project root directory: ' + config.scope
+    )
+    console.assert(
+      fs.existsSync(config.scope),
+      '[husky] Scope path is not exists: ' + config.scope
+    )
+
+    if (config.scope === projectDir) {
+      config.scope = ''
+    }
+  }
+
+  const hookScript = getHookScript(hookName, relativePath, cmd, config)
 
   // Create hooks directory if needed
   if (!fs.existsSync(hooksDir)) fs.mkdirSync(hooksDir)
