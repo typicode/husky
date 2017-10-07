e8e170236b987cd0c29f97f8fdce74addb6e5ddc
typicode <typicode@gmail.com>
2017-10-07 22:28:53 +0200

    fix

--- template/hook.sh
+++ template/hook.sh
@@ -2,7 +2,7 @@
 __huskyIdentifier__
 
 hookname=`basename "$0"`
-[ -f package.json ] && cat package.json | grep -q "\\"$hookname\\"[[:space:]]*:"
+[ -f package.json ] && cat package.json | grep -q "\"$hookname\"[[:space:]]*:"
 
 if [[ $? -eq 0 ]]; then
   ./node_modules/.bin/run-node ./node_modules/husky/lib/run $hookname
