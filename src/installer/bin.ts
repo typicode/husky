1f8ecf1ed8809de7cdeb92253ae5d30435027048
typicode <typicode@gmail.com>
2017-10-10 22:00:17 +0200

    update

--- src/installer/bin.ts
+++ src/installer/bin.ts
@@ -1,27 +1,24 @@
-import * as execa from "execa";
-import * as fs from "fs";
-import * as path from "path";
-import { install, uninstall } from "./";
+import * as execa from 'execa'
+import * as fs from 'fs'
+import * as path from 'path'
+import { install, uninstall } from './'
 
 // Action can be "install" or "uninstall"
 // huskyDir is ONLY used in dev, don't use this arguments
-const [, , action, huskyDir = path.join(__dirname, "../..")] = process.argv;
+const [, , action, huskyDir = path.join(__dirname, '../..')] = process.argv
 
 // Find Git dir
-const { status, stdout, stderr } = execa.sync("git", [
-  "rev-parse",
-  "--git-dir"
-]);
-const gitDir = path.resolve(stdout); // Needed to normalize path on Windows
+const { status, stdout, stderr } = execa.sync('git', ['rev-parse', '--git-dir'])
+const gitDir = path.resolve(stdout) // Needed to normalize path on Windows
 
 if (status !== 0) {
-  console.log(stderr);
-  process.exit(1);
+  console.log(stderr)
+  process.exit(1)
 }
 
 // Run installer
-if (action === "install") {
-  install(gitDir, huskyDir);
+if (action === 'install') {
+  install(gitDir, huskyDir)
 } else {
-  uninstall(gitDir, huskyDir);
+  uninstall(gitDir, huskyDir)
 }
