33dc86ff4bfda0d18aeb6ce346ea3e00b3c23802
typicode <typicode@gmail.com>
2018-02-07 00:42:00 +0100

    Update

--- ci-check.js
+++ ci-check.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
-if (fs.existsSync('pre-commit.ok')) {
-  fs.unlinkSync('pre-commit.ok')
-  console.log('.git/hooks/pre-commit script has run successfully on CI')
+if (fs.existsSync('ci.ok')) {
+  fs.unlinkSync('ci.ok')
+  console.log('.git/hooks/post-checkout script has run successfully on CI')
 } else {
-  console.log('.git/hooks/pre-commit script has failed running on CI')
+  console.log('.git/hooks/post-checkout script has failed running on CI')
   process.exit(1)
 }
