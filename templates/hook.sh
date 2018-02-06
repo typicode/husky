d70b48b2973996754912f9995f85787e344b5270
typicode <typicode@gmail.com>
2018-02-06 23:05:39 +0100

    Travis test

--- templates/hook.sh
+++ templates/hook.sh
@@ -4,9 +4,4 @@
 
 export GIT_PARAMS="$*"
 
-hookname=`basename "$0"`
-
-if [ -f package.json ] && cat package.json | grep -q "\"$hookname\"[[:space:]]*:"; then
-  {node} ./node_modules/husky/lib/runner/bin $hookname
-  exit $?
-fi 
+{node} ./node_modules/husky/lib/runner/bin `basename "$0"`
