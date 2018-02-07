52ec573ba78658745c4df28cd4db0ad4e6b1c499
typicode <typicode@gmail.com>
2018-02-07 01:04:33 +0100

    Update devinstall

--- scripts/dev.js
+++ scripts/dev.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 // Fix paths
 fs.readdirSync('.git/hooks').map(filename => {
-  console.log('update', filename)
+  console.log('devinstall: update', filename)
   const absoluteFilename = path.join('.git/hooks', filename)
   const data = fs.readFileSync(absoluteFilename, 'utf-8')
   const updatedData = data.replace('./node_modules/husky/', './')
