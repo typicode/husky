642baceec5ab5d2730a822ccae9b04c85e2b0b9b
Andrey <insonifi@gmail.com>
2019-01-16 09:54:28 +0100

    Don't execute shell as a programme explicitly (#10)
    https://docs.python.org/3.7/library/subprocess.html#subprocess.Popen
    For both Windows and POSIX systems it works more predictable with
    a default value of `shell=False`

--- src/utils/get-hook-script.js
+++ src/utils/get-hook-script.js
@@ -183,9 +183,9 @@ function createHgScript(normalizedPath, hookName, npmScriptName, verifyMessage)
           try:
             FNULL = os.open(os.devnull, os.O_WRONLY)
             if show_output:
-              return subprocess.check_call(cmd, stdin=FNULL, shell=True) == 0
+              return subprocess.check_call(cmd, stdin=FNULL) == 0
             else:
-              return subprocess.check_call(cmd, stdin=FNULL, stdout=FNULL, stderr=FNULL, shell=True) == 0
+              return subprocess.check_call(cmd, stdin=FNULL, stdout=FNULL, stderr=FNULL) == 0
           except (OSError, subprocess.CalledProcessError) as e:
             print_error_msg(str(e))
             return False
