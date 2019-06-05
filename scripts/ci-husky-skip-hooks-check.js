71316ffdda51db6bce76d9d7e6810b8f7b50018d
typicode <typicode@gmail.com>
2019-06-05 15:07:45 +0200

    Refactor CI tests

--- scripts/ci-husky-skip-hooks-check.js
+++ scripts/ci-husky-skip-hooks-check.js
@@ -5,14 +5,6 @@ const path = require('path')
 const filename = path.join(__dirname, '../ci-post-checkout')
 
 if (fs.existsSync(filename)) {
-  const data = fs.readFileSync(filename, 'utf-8')
-
-  if (data.split(' ').length !== 3) {
-    console.log('Not all params were set in HUSKY_GIT_PARAMS')
-    console.log('Got', data)
-    process.exit(1)
-  }
-
   fs.unlinkSync(filename)
   console.log(
     '.git/hooks/post-checkout script has run, hooks were not skipped.'
