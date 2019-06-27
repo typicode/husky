086a5e064db026fd20d322c09cf837d400cade80
typicode <typicode@gmail.com>
2019-06-27 10:40:13 +0200

    use absolute git dir (#516)

--- src/installer/__tests__/gitRevParse.ts
+++ src/installer/__tests__/gitRevParse.ts
@@ -2,12 +2,15 @@ import path from 'path'
 import slash from 'slash'
 import gitRevParse from '../gitRevParse'
 
+const root = path.join(__dirname, '../../..')
+
 describe('gitRevParse', (): void => {
   it('should return topLevel and gitDir', (): void => {
     expect(gitRevParse()).toStrictEqual({
-      // Normalize
-      topLevel: slash(path.join(__dirname, '../../..')),
-      gitDir: '.git'
+      // Git rev-parse uses a different separator on Linux/MacOS and Windows
+      // slash is used to normalized the returned value for tests
+      topLevel: slash(root),
+      gitDir: slash(path.join(root, '.git'))
     })
   })
 })
--- src/installer/gitRevParse.ts
+++ src/installer/gitRevParse.ts
@@ -2,16 +2,14 @@ import slash from 'slash'
 import execa from 'execa'
 
 export default function(): {
-  // Show the absolute path of the top-level directory.
   topLevel: string
-  // Show $GIT_COMMON_DIR if defined, else $GIT_DIR.
   gitDir: string
 } {
   try {
     const { stdout } = execa.sync('git', [
       'rev-parse',
       '--show-toplevel',
-      '--git-common-dir'
+      '--absolute-git-dir'
     ])
 
     const [topLevel, gitDir] = stdout
