c315fe5b0ab297c3d553ed00cab943261bc75841
typicode <typicode@gmail.com>
2019-07-29 18:54:47 +0200

    wip

---
+++ scripts/README.md
@@ -0,0 +1,1 @@
+Scripts related to tests
--- scripts/test-install.sh
+++ scripts/test-install.sh
@@ -1,12 +1,34 @@
-testPath=/tmp/husky-test
+projectPath=/tmp/husky-project
 
-rm -rf $testPath && mkdir $testPath
-cp scripts/.huskyrc $testPath
-npm run build && npm pack && mv husky-*.tgz $testPath
+# Reset dir
+rm -rf $projectPath && mkdir $projectPath
 
-cd $testPath
+# Copy test config
+cp scripts/.huskyrc $projectPath
 
+# Husky needs to be packed to be closer to a real install
+npm run build && npm pack
+
+# Move husky to project
+mv husky-*.tgz $projectPath
+
+# Init a blank git/npm project and install husky
+cd $projectPath
 git init
 npm init -y
 npm install husky-*.tgz
+
+# Show post-checkout hook content
+cat .git/hooks/post-checkout
+
+# Test HUSKY_SKIP_HOOKS
+HUSKY_SKIP_HOOKS=1 time git checkout
+if [-f ci-post-checkout] then;
+  echo ".git/hooks/post-checkout script has run, hooks were not skipped."
+  exit 1
+fi
+
+# Should not fail due to missing script
+rm -rf node_modules
+time git checkout
 
