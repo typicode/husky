b6616739f59840e12d855ce8996e02e5b076db02
Kristoffer K <merceyz@users.noreply.github.com>
2020-01-10 04:30:20 +0100

    fix: use process.exit if install fails (#633)
    * fix: provide an exitCode if install fails
    
    * Update src/installer/bin.ts
    
    Co-Authored-By: Maël Nison <nison.mael@gmail.com>
    
    Co-authored-by: Maël Nison <nison.mael@gmail.com>

--- src/installer/bin.ts
+++ src/installer/bin.ts
@@ -103,6 +103,7 @@ function run(): void {
     console.log(chalk.red(err.message.trim()))
     debug(err.stack)
     console.log(chalk.red(`husky > Failed to ${action}`))
+    process.exitCode = 1
   }
 }
 
