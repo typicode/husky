a458c6042d33e332673c40eca025484382788c00
zhangciwu <conanla@qq.com>
2020-04-01 14:25:17 +0800

    fix: lint

--- bin/run.js
+++ bin/run.js
@@ -6,10 +6,14 @@ const pkg = require('../package.json')
 // Node version isn't supported, skip
 pleaseUpgradeNode(pkg, {
   message(requiredVersion) {
-    return 'Husky requires Node ' + 
+    return (
+      'Husky requires Node ' +
       requiredVersion +
-      ' (runtime: ' + process.version + ')' + 
+      ' (runtime: ' +
+      process.version +
+      ')' +
       ", can't run Git hook."
+    )
   }
 })
 
