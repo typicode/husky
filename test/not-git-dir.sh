f8f1afe9206b4beb66080d6b2cc2a1db6ba4e1fa
typicode <typicode@gmail.com>
2021-02-08 18:39:55 +0100

    fix(install): do not fail if not inside a Git directory

---
+++ test/not-git-dir.sh
@@ -0,0 +1,13 @@
+# shellcheck shell=bash
+
+# shellcheck source=./_functions.sh
+. "$(dirname "$0")/_functions.sh"
+
+title "not git dir"
+tempDir="/tmp/husky-not-git-dir"
+
+rm -rf $tempDir
+cd_and_install_tgz $tempDir
+
+# Should not fail
+npx --no-install husky install && echo -e "\e[0;32mOK\e[m"
