e1f4a824e875a57ea0478ce26c90c812fb358f01
typicode <typicode@gmail.com>
2021-04-01 22:42:36 +0200

    feat: append command

--- packages/husky-init/src/index.ts
+++ packages/husky-init/src/index.ts
@@ -1,19 +1,34 @@
 import { PackageJson } from 'type-fest'
 
+function appendScript(pkg: PackageJson, scriptName: string, cmd: string) {
+  pkg.scripts ||= {}
+  if (pkg.scripts[scriptName] !== undefined) {
+    if (pkg.scripts[scriptName].includes(cmd)) {
+      console.log(`  "${cmd}" command already exists in ${scriptName} script, skipping.`)
+    } else {
+      console.log(`  appending "${cmd}" command to ${scriptName} script`)
+      pkg.scripts[scriptName] += ` && ${cmd}`
+    }
+  } else {
+    console.log(`  setting ${scriptName} script to command "${cmd}"`)
+    pkg.scripts[scriptName] = cmd
+  }
+}
+
 export function updatePkg(pkg: PackageJson, isYarn2: boolean): PackageJson {
   pkg.devDependencies ||= {}
   pkg.devDependencies.husky = '^6.0.0'
   if (isYarn2) {
     pkg.scripts ||= {}
-    pkg.scripts.postinstall = 'husky install'
+    appendScript(pkg, 'postinstall', 'husky install')
     if (pkg.private !== true) {
-      pkg.scripts.prepublishOnly = 'pinst --disable'
-      pkg.scripts.postpublish = 'pinst --enable'
+      appendScript(pkg, 'prepublishOnly', 'pinst --disable')
+      appendScript(pkg, 'postpublish', 'pinst --enable')
       pkg.devDependencies.pinst = '^2.0.0'
     }
   } else {
     pkg.scripts ||= {}
-    pkg.scripts.prepare = 'husky install'
+    appendScript(pkg, 'prepare', 'husky install')
   }
 
   return pkg
