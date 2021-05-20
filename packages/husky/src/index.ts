2945a33cf29177b420816d935a4f6c87b6c3fa40
typicode <typicode@gmail.com>
2021-05-20 11:56:41 +0200

    feat: use esm

--- packages/husky/src/index.ts
+++ packages/husky/src/index.ts
@@ -1,6 +1,12 @@
-import fs = require('fs')
-import cp = require('child_process')
-import path = require('path')
+import { spawnSync } from 'child_process'
+import {
+  appendFileSync,
+  copyFileSync,
+  existsSync,
+  mkdirSync,
+  writeFileSync,
+} from 'fs'
+import { dirname, join, resolve } from 'path'
 
 function l(msg: string): void {
   console.log(`husky - ${msg}`)
@@ -8,7 +14,7 @@ function l(msg: string): void {
 
 export function install(dir = '.husky'): void {
   // Ensure that we're inside a git repository
-  if (cp.spawnSync('git', ['rev-parse']).status !== 0) {
+  if (spawnSync('git', ['rev-parse']).status !== 0) {
     l('not a Git repository, skipping hooks installation')
     return
   }
@@ -17,30 +23,27 @@ export function install(dir = '.husky'): void {
   const url = 'https://typicode.github.io/husky/#/?id=custom-directory'
 
   // Ensure that we're not trying to install outside of cwd
-  if (!path.resolve(process.cwd(), dir).startsWith(process.cwd())) {
+  if (!resolve(process.cwd(), dir).startsWith(process.cwd())) {
     throw new Error(`.. not allowed (see ${url})`)
   }
 
   // Ensure that cwd is git top level
-  if (!fs.existsSync('.git')) {
+  if (!existsSync('.git')) {
     throw new Error(`.git can't be found (see ${url})`)
   }
 
   try {
     // Create .husky/_
-    fs.mkdirSync(path.join(dir, '_'), { recursive: true })
+    mkdirSync(join(dir, '_'), { recursive: true })
 
     // Create .husky/.gitignore
-    fs.writeFileSync(path.join(dir, '.gitignore'), '_\n')
+    writeFileSync(join(dir, '.gitignore'), '_\n')
 
     // Copy husky.sh to .husky/_/husky.sh
-    fs.copyFileSync(
-      path.join(__dirname, 'husky.sh'),
-      path.join(dir, '_/husky.sh'),
-    )
+    copyFileSync(join(__dirname, 'husky.sh'), join(dir, '_/husky.sh'))
 
     // Configure repo
-    const { error } = cp.spawnSync('git', ['config', 'core.hooksPath', dir])
+    const { error } = spawnSync('git', ['config', 'core.hooksPath', dir])
     if (error) {
       throw error
     }
@@ -53,14 +56,14 @@ export function install(dir = '.husky'): void {
 }
 
 export function set(file: string, cmd: string): void {
-  const dir = path.dirname(file)
-  if (!fs.existsSync(dir)) {
+  const dir = dirname(file)
+  if (!existsSync(dir)) {
     throw new Error(
       `can't create hook, ${dir} directory doesn't exist (try running husky install)`,
     )
   }
 
-  fs.writeFileSync(
+  writeFileSync(
     file,
     `#!/bin/sh
 . "$(dirname "$0")/_/husky.sh"
@@ -74,8 +77,8 @@ ${cmd}
 }
 
 export function add(file: string, cmd: string): void {
-  if (fs.existsSync(file)) {
-    fs.appendFileSync(file, `${cmd}\n`)
+  if (existsSync(file)) {
+    appendFileSync(file, `${cmd}\n`)
     l(`updated ${file}`)
   } else {
     set(file, cmd)
@@ -83,7 +86,7 @@ export function add(file: string, cmd: string): void {
 }
 
 export function uninstall(): void {
-  cp.spawnSync('git', ['config', '--unset', 'core.hooksPath'], {
+  spawnSync('git', ['config', '--unset', 'core.hooksPath'], {
     stdio: 'inherit',
   })
 }
