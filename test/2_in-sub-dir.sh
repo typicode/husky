399de82a5d6e153e320978984000426a71920447
typicode <typicode@gmail.com>
2021-07-01 06:23:58 +0200

    feat: husky 7

---
+++ test/2_in-sub-dir.sh
@@ -0,0 +1,15 @@
+. "$(dirname "$0")/functions.sh"
+setup
+install
+
+# Test custom dir support
+mkdir sub
+npx --no-install husky install sub/husky
+npx --no-install husky add sub/husky/pre-commit "echo \"pre-commit\" && exit 1"
+
+# Test core.hooksPath
+expect_hooksPath_to_be "sub/husky"
+
+# Test pre-commit
+git add package.json
+expect 1 "git commit -m foo"
