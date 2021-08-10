c5a3342c389b861a8a24f33d5ae565655fab4530
Andrey Ivlev <andreyivlev@pm.me>
2021-08-10 16:38:31 +0300

    Fix pre-commit hook in WebStorm

--- husky.sh
+++ husky.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 if [ -z "$husky_skip_init" ]; then
   debug () {
-    [ "$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"
+    if [ "$HUSKY_DEBUG" = "1" ]; then
+      echo "husky (debug) - $1"
+    fi
   }
 
   readonly hook_name="$(basename "$0")"
