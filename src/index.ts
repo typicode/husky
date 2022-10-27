8aba34cbe5cc371464c541edc05c176913a335f0
Jakob Deutsch <Jakob.G.Deutsch@aexp.com>
2022-10-27 14:46:39 -0400

    chore: added git not installed error message

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ export function install(dir = '.husky'): void {
   // If git command is not found, status is null and we should return.
   // That's why status value needs to be checked explicitly.
   if (git(['rev-parse']).status !== 0) {
+    l(`git must be installed on your machine`)
     return
   }
 
