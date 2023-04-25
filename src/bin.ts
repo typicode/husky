ed38fcafcb19a13c980b06903757eda77375078a
typicode <typicode@gmail.com>
2023-04-25 23:42:23 +0200

    chore: lint fix

--- src/bin.ts
+++ src/bin.ts
@@ -4,11 +4,11 @@ import h = require('./')
 
 /** Show usage and exit with given code */
 function help(code: number) {
-    console.log(`Usage:
+  console.log(`Usage:
     husky install [dir] (default: .husky)
     husky uninstall
     husky set|add <file> [cmd]`)
-    process.exit(code)
+  process.exit(code)
 }
 
 // Get CLI arguments
@@ -19,25 +19,25 @@ const [x, y] = args
 
 /** Set or add command in hook */
 const hook = (fn: (a1: string, a2: string) => void) => (): void =>
-    // Show usage if no or more than 2 arguments are provided
-    !ln || ln > 2 ? help(2) : fn(x, y)
+  // Show usage if no or more than 2 arguments are provided
+  !ln || ln > 2 ? help(2) : fn(x, y)
 
 /** CLI commands */
 const cmds: { [key: string]: () => void } = {
-    install: (): void => (ln > 1 ? help(2) : h.install(x)),
-    uninstall: h.uninstall,
-    set: hook(h.set),
-    add: hook(h.add),
-    ['-v']: () =>
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
-        console.log(require(p.join(__dirname, '../package.json')).version),
+  install: (): void => (ln > 1 ? help(2) : h.install(x)),
+  uninstall: h.uninstall,
+  set: hook(h.set),
+  add: hook(h.add),
+  ['-v']: () =>
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
+    console.log(require(p.join(__dirname, '../package.json')).version),
 }
 
 // Run CLI
 try {
-    // Run command or show usage for unknown command
-    cmds[cmd] ? cmds[cmd]() : help(0)
+  // Run command or show usage for unknown command
+  cmds[cmd] ? cmds[cmd]() : help(0)
 } catch (e) {
-    console.error(e instanceof Error ? `husky - ${e.message}` : e)
-    process.exit(1)
+  console.error(e instanceof Error ? `husky - ${e.message}` : e)
+  process.exit(1)
 }
--- src/index.ts
+++ src/index.ts
@@ -7,95 +7,92 @@ const l = (msg: string): void => console.log(`husky - ${msg}`)
 
 /** Execute a git command */
 const git = (args: string[]): cp.SpawnSyncReturns<Buffer> =>
-    cp.spawnSync('git', args, { stdio: 'inherit' })
+  cp.spawnSync('git', args, { stdio: 'inherit' })
 
 /** Check prerequisites, install and configure husky */
 export function install(dir = '.husky'): void {
-    if (process.env.HUSKY === '0') {
-        l('HUSKY env variable is set to 0, skipping install')
-        return
+  if (process.env.HUSKY === '0') {
+    l('HUSKY env variable is set to 0, skipping install')
+    return
+  }
+
+  // Ensure that we're inside a Git repository.
+  // If git command is not found, status is null and we should return.
+  // That's why the status value needs to be checked explicitly.
+  if (git(['rev-parse']).status !== 0) {
+    l(`git command not found, skipping install`)
+    return
+  }
+
+  /** Custom dir help */
+  const url = 'https://typicode.github.io/husky/#/?id=custom-directory'
+
+  // Ensure that we're not trying to install outside of cwd
+  if (!p.resolve(process.cwd(), dir).startsWith(process.cwd())) {
+    throw new Error(`.. not allowed (see ${url})`)
+  }
+
+  // Ensure that cwd is git top level
+  if (!fs.existsSync('.git')) {
+    throw new Error(`.git can't be found (see ${url})`)
+  }
+
+  try {
+    // Create .husky/_
+    fs.mkdirSync(p.join(dir, '_'), { recursive: true })
+
+    // Create .husky/_/.gitignore
+    fs.writeFileSync(p.join(dir, '_/.gitignore'), '*')
+
+    // Copy husky.sh to .husky/_/husky.sh
+    fs.copyFileSync(p.join(__dirname, '../husky.sh'), p.join(dir, '_/husky.sh'))
+
+    // Configure repo
+    const { error } = git(['config', 'core.hooksPath', dir])
+    if (error) {
+      throw error
     }
+  } catch (e) {
+    l('Git hooks failed to install')
+    throw e
+  }
 
-    // Ensure that we're inside a Git repository.
-    // If git command is not found, status is null and we should return.
-    // That's why the status value needs to be checked explicitly.
-    if (git(['rev-parse']).status !== 0) {
-        l(`git command not found, skipping install`)
-        return
-    }
-
-    /** Custom dir help */
-    const url = 'https://typicode.github.io/husky/#/?id=custom-directory'
-
-    // Ensure that we're not trying to install outside of cwd
-    if (!p.resolve(process.cwd(), dir).startsWith(process.cwd())) {
-        throw new Error(`.. not allowed (see ${url})`)
-    }
-
-    // Ensure that cwd is git top level
-    if (!fs.existsSync('.git')) {
-        throw new Error(`.git can't be found (see ${url})`)
-    }
-
-    try {
-        // Create .husky/_
-        fs.mkdirSync(p.join(dir, '_'), { recursive: true })
-
-        // Create .husky/_/.gitignore
-        fs.writeFileSync(p.join(dir, '_/.gitignore'), '*')
-
-        // Copy husky.sh to .husky/_/husky.sh
-        fs.copyFileSync(
-            p.join(__dirname, '../husky.sh'),
-            p.join(dir, '_/husky.sh'),
-        )
-
-        // Configure repo
-        const { error } = git(['config', 'core.hooksPath', dir])
-        if (error) {
-            throw error
-        }
-    } catch (e) {
-        l('Git hooks failed to install')
-        throw e
-    }
-
-    l('Git hooks installed')
+  l('Git hooks installed')
 }
 
 /** Add a new hook or override an existing hook */
 export function set(file: string, cmd: string): void {
-    const dir = p.dirname(file)
-    if (!fs.existsSync(dir)) {
-        throw new Error(
-            `can't create hook, ${dir} directory doesn't exist (try running 'husky install')`,
-        )
-    }
+  const dir = p.dirname(file)
+  if (!fs.existsSync(dir)) {
+    throw new Error(
+      `can't create hook, ${dir} directory doesn't exist (try running 'husky install')`,
+    )
+  }
 
-    fs.writeFileSync(
-        file,
-        `#!/usr/bin/env sh
+  fs.writeFileSync(
+    file,
+    `#!/usr/bin/env sh
 . "$(dirname -- "$0")/_/husky.sh"
 
 ${cmd}
 `,
-        { mode: 0o0755 },
-    )
+    { mode: 0o0755 },
+  )
 
-    l(`created ${file}`)
+  l(`created ${file}`)
 }
 
 /** Add a new hook or append a command to an existing hook */
 export function add(file: string, cmd: string): void {
-    if (fs.existsSync(file)) {
-        fs.appendFileSync(file, `${cmd}\n`)
-        l(`updated ${file}`)
-    } else {
-        set(file, cmd)
-    }
+  if (fs.existsSync(file)) {
+    fs.appendFileSync(file, `${cmd}\n`)
+    l(`updated ${file}`)
+  } else {
+    set(file, cmd)
+  }
 }
 
 /** Uninstall husky */
 export function uninstall(): void {
-    git(['config', '--unset', 'core.hooksPath'])
+  git(['config', '--unset', 'core.hooksPath'])
 }
