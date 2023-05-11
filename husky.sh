da44f7b07c4607001da997d36cfbf7f9ec926c53
typicode <typicode@gmail.com>
2023-05-11 11:42:07 +0200

    feat: use zsh

--- husky.sh
+++ husky.sh
@@ -1,36 +1,41 @@
 #!/usr/bin/env sh
 if [ -z "$husky_skip_init" ]; then
-  debug () {
-    if [ "$HUSKY_DEBUG" = "1" ]; then
-      echo "husky (debug) - $1"
-    fi
-  }
+	debug() {
+		if [ "$HUSKY_DEBUG" = "1" ]; then
+			echo "husky (debug) - $1"
+		fi
+	}
 
-  readonly hook_name="$(basename -- "$0")"
-  debug "starting $hook_name..."
+	readonly hook_name="$(basename -- "$0")"
+	debug "starting $hook_name..."
 
-  if [ "$HUSKY" = "0" ]; then
-    debug "HUSKY env variable is set to 0, skipping hook"
-    exit 0
-  fi
+	if [ "$HUSKY" = "0" ]; then
+		debug "HUSKY env variable is set to 0, skipping hook"
+		exit 0
+	fi
 
-  if [ -f ~/.huskyrc ]; then
-    debug "sourcing ~/.huskyrc"
-    . ~/.huskyrc
-  fi
+	if [ -f ~/.huskyrc ]; then
+		debug "sourcing ~/.huskyrc"
+		. ~/.huskyrc
+	fi
 
-  readonly husky_skip_init=1
-  export husky_skip_init
-  sh -e "$0" "$@"
-  exitCode="$?"
+	readonly husky_skip_init=1
+	export husky_skip_init
 
-  if [ $exitCode != 0 ]; then
-    echo "husky - $hook_name hook exited with code $exitCode (error)"
-  fi
+	if [ "$(basename -- "$SHELL")" = "zsh" ]; then
+		zsh --emulate sh -e "$0" "$@"
+	else
+		sh -e "$0" "$@"
+	fi
+	exitCode="$?"
 
-  if [ $exitCode = 127 ]; then
-    echo "husky - command not found in PATH=$PATH"
-  fi
+	if [ $exitCode != 0 ]; then
+		echo "husky - $hook_name hook exited with code $exitCode (error)"
+	fi
 
-  exit $exitCode
+	if [ $exitCode = 127 ]; then
+		echo "husky - command not found in PATH=$PATH"
+	fi
+
+	exit $exitCode
 fi
