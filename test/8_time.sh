aaf37fe1879c755b36527b7a03d06b72844b11cd
typicode <typicode@gmail.com>
2023-09-27 15:21:56 +0200

    test: time

---
+++ test/8_time.sh
@@ -0,0 +1,17 @@
+. "$(dirname -- "$0")/functions.sh"
+setup
+install
+
+npx --no-install husky install
+
+# Test core.hooksPath
+expect_hooksPath_to_be ".husky"
+
+# Test pre-commit
+git add package.json
+npx --no-install husky add .husky/pre-commit "echo \"pre-commit\" && exit 1"
+time expect 1 "git commit -m foo"
+
+# Uninstall
+npx --no-install husky uninstall
+expect 1 "git config core.hooksPath"
