c1bbd9b8583d900171b6c81d751f11e506d12c1e
typicode <typicode@gmail.com>
2024-01-26 00:41:29 +0100

    style: indent

--- bin.js
+++ bin.js
@@ -5,14 +5,14 @@ import i from './index.js'
 let a = process.argv[2]
 
 if (a == 'init') {
-  let p = 'package.json'
-  let d = JSON.parse(f.readFileSync(p))
-  ;(d.scripts ||= {}).prepare = 'husky'
-  w('package.json', JSON.stringify(d, null, /\t/.test() ? '\t' : 2) + '\n')
-  process.stdout.write(i())
-	try { f.mkdirSync('.husky') } catch {}
-  w('.husky/pre-commit', process.env.npm_config_user_agent.split('/')[0] + ' test\n')
-  process.exit()
+	let p = 'package.json'
+	let d = JSON.parse(f.readFileSync(p))
+		; (d.scripts ||= {}).prepare = 'husky'
+	w('package.json', JSON.stringify(d, null, /\t/.test() ? '\t' : 2) + '\n')
+	process.stdout.write(i())
+	try { f.mkdirSync('.husky') } catch { }
+	w('.husky/pre-commit', process.env.npm_config_user_agent.split('/')[0] + ' test\n')
+	process.exit()
 }
 
 let d = c => console.error(`${c} command is deprecated`)
