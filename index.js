9891ace6454b111bccf6d38860f6ce08c4424a5d
typicode <typicode@gmail.com>
2024-07-29 16:55:02 +0200

    clarify deprecation message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import f, { readdir, writeFileSync as w } from 'fs'
 import p from 'path'
 
 let l = [ 'pre-commit', 'prepare-commit-msg', 'commit-msg', 'post-commit', 'applypatch-msg', 'pre-applypatch', 'post-applypatch', 'pre-rebase', 'post-rewrite', 'post-checkout', 'post-merge', 'pre-push', 'pre-auto-gc' ],
-	msg = `echo "husky - DEPRECATED\n\nPlease remove the following lines from your hook scripts:\n\n#!/usr/bin/env sh\n. \\"\\$(dirname -- \\"\\$0\\")/_/husky.sh\\"\n\nThey WILL FAIL in v10.0.0\n"`
+	msg = `echo "husky - DEPRECATED\n\nPlease remove the following two lines from $0:\n\n#!/usr/bin/env sh\n. \\"\\$(dirname -- \\"\\$0\\")/_/husky.sh\\"\n\nThey WILL FAIL in v10.0.0\n"`
 
 export default (d = '.husky') => {
 	if (process.env.HUSKY === '0') return 'HUSKY=0 skip install'
